bacd4af086270e7446e8e1a41862a2c6e0466851
unknown <Jsbbvk@gmail.com>
2018-06-25 23:30:19 -0700

    added LOGO and color changing

---
+++ js/changingColorScheme.js
@@ -0,0 +1,68 @@
+const randomColor = () => '#' + Math.random().toString(16).substr(-6)
+const changeColor = () => {
+    var color = randomColor();
+    document.getElementById("sideNav").style.backgroundColor = color;
+    document.getElementById("jzh1").style.color = color;
+    document.getElementById("jzh2").style.color = color;
+    document.getElementById("jzh3").style.color = color;
+
+    var dColor = ColorLuminance(color, -0.5);
+    document.getElementById("aboutA").style.backgroundColor = dColor;
+    document.getElementById("awaA").style.backgroundColor = dColor;
+    document.getElementById("intA").style.backgroundColor = dColor;
+    document.getElementById("skillA").style.backgroundColor = dColor;
+    document.getElementById("expA").style.backgroundColor = dColor;
+    document.getElementById("eduA").style.backgroundColor = dColor;
+}
+
+
+
+function ColorLuminance(hex, lum) {
+
+    // validate hex string
+    hex = String(hex).replace(/[^0-9a-f]/gi, '');
+    if (hex.length < 6) {
+        hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+    }
+    lum = lum || 0;
+
+    // convert to decimal and change luminosity
+    var rgb = "#", c, i;
+    for (i = 0; i < 3; i++) {
+        c = parseInt(hex.substr(i*2,2), 16);
+        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+        rgb += ("00"+c).substr(c.length);
+    }
+
+    return rgb;
+}
+
+document.getElementById("profileHolder").addEventListener("click", function() {
+    playLogoAnim();
+});
+
+function playLogoAnim() {
+    document.getElementById("jzh1").classList.remove("anim");
+    void document.getElementById("jzh1").offsetWidth;
+    document.getElementById("jzh1").classList.add("anim");
+
+    document.getElementById("jzh2").classList.remove("anim");
+    void document.getElementById("jzh2").offsetWidth;
+    document.getElementById("jzh2").classList.add("anim");
+
+    document.getElementById("jzh3").classList.remove("anim");
+    void document.getElementById("jzh3").offsetWidth;
+    document.getElementById("jzh3").classList.add("anim");
+}
+
+function init() {
+    setInterval(() => {
+        changeColor()
+    }, 5000)
+
+
+    playLogoAnim();
+    changeColor();
+}
+
+init();
