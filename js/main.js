70534963aadd8adb5b2d2a95701bc8ca34b6c28d
Jsbbvk <jsbbvk@gmail.com>
2020-08-25 17:03:45 -0400

    new change

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const PROFILE_IMAGE_CHANGE_TIME = 5000;
-
+const MAX_PROFILE_PICS = 2;
 $(document).ready(function () {
   let pImageCounter = 1;
-  setInterval(() => {
+
+  function changeProfilePic() {
     $("#profile-image-wrapper").css(
-      "background-image",
-      "url(img/profile" + pImageCounter++ + ".jpg)"
+        "background-image",
+        "url(img/profile" + pImageCounter++ + ".jpg)"
     );
-    if (pImageCounter >= 3) pImageCounter = 1;
-  }, PROFILE_IMAGE_CHANGE_TIME);
+    if (pImageCounter > MAX_PROFILE_PICS) pImageCounter = 1;
+  }
+  setInterval(changeProfilePic, PROFILE_IMAGE_CHANGE_TIME);
+
 });
